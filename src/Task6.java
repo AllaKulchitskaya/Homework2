4220ad490af3d96e3d449bae094600409f56dc64
AllaKulchitskaya <alla070@list.ru>
2022-10-31 20:07:02 +0300

    Задача 6

---
+++ src/Task6.java
@@ -0,0 +1,11 @@
+public class Task6 {
+    public static void main(String[] args) {
+        var boxerWeight1 = 78.2;
+        var boxerWeight2 = 82.7;
+        var totalWeight = boxerWeight1 + boxerWeight2;
+        System.out.println("Общий вес двух боксёров равен " + totalWeight + " кг");
+        var weightDifference = boxerWeight2 - boxerWeight1;
+        System.out.println("Разница в весе двух боксёров равна " + weightDifference + " кг");
+
+    }
+}
